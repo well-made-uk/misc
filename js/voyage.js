c385b091c05771245d21d48ef18026fc7097daaf
Ben Wilde <ben-wilde@hey.com>
2022-03-09 14:38:17 +0000

    a

--- js/voyage.js
+++ js/voyage.js
@@ -1,43 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-  jQuery('.menu-toggler').on('click',setTimeout(mobileMenu,200));
+jQuery(document).ready(function() {
+  setTimeout(mobileMenu,200);
   jQuery('.elementor-nav-menu').on('hover',setTimeout(desktopMenu,200));
-  
-  cssel = document.createElement('style', {id: 'menuStyle'});
-  document.body.appendChild(cssel);
+  jQuery(document).append('<style ="menuStyle"></style>');
+  const styleStuff = jQuery("#menuStyle");
 })
 
 function desktopMenu() {
   // Get height of elements
-  totalHeight = document.querySelector('.elementor-location-header').offsetHeight();
+  totalHeight = $('.elementor-location-header').height();
         
   // Add CSS
   css = "#nav-menu-main .sub-menu[style] {top:" + totalHeight + "px!important}";
-  document.getElementById('menuStyle').innerHTML = css
+  styleStuff.innerHTML = css;
 }
 
 function mobileMenu() {
   // Add mobile menu icon
-  i = document.createElement('i',{style: 'display:none',class: 'fa fa-caret-down opener',onClick: "opener"})
-  document.querySelector('#mobile-menu-nav .menu-item-has-children').insertBefore(i, document.querySelector('#mobile-menu-nav .menu-item-has-children').firstChild);
+  jQuery('#mobile-menu-nav .menu-item-has-children').prepend('<i style="display:none" class="fa fa-caret-down opener" onClick="opener"></i>');
         
   // Hide mobile submenu by default
-  document.querySelector('#mobile-menu-nav .menu-item-has-children .sub-menu').toggle(this);
+  jQuery('#mobile-menu-nav .menu-item-has-children .sub-menu').slideToggle(200);
 }
 
 // Function for opening the mobile submenu
 function opener() {
-    document.querySelector('#mobile-menu-nav .menu-item-has-children .sub-menu').toggle(this);
+    jQuery('#mobile-menu-nav .menu-item-has-children .sub-menu').slideToggle(200);
 }
-
-function toggle(elem) {
-
-	// If the element is visible, hide it
-	if (window.getComputedStyle(elem).display === 'block') {
-		hide(elem);
-		return;
-	}
-
-	// Otherwise, show it
-	show(elem);
-
-};
