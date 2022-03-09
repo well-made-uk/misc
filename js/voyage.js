77a43f4d480a48e3301117909e1f7874d9139b15
Ben Wilde <ben-wilde@hey.com>
2022-03-09 13:42:00 +0000

    Update voyage.js

--- js/voyage.js
+++ js/voyage.js
@@ -1,18 +1,22 @@
 jQuery('document').ready(function($) {
+  
+  $('.menu-toggler').click(mobileMenu());
   // Get height of elements
   const totalHeight = $('.elementor-location-header').height();
         
   // Add CSS
   const css = "<style>#nav-menu-main .sub-menu[style] {top:" + totalHeight + "px!important}</style>";
   $('body').append(css);
-        
+})
+
+function mobileMenu() {
   // Add mobile menu icon
-  $('#mobile-menu-nav .menu-item-281').prepend('<i style="display:none" class="fa fa-caret-down opener" onCLick="opener()"></i>');
+  jQuery('#mobile-menu-nav .menu-item-281').prepend('<i style="display:none" class="fa fa-caret-down opener" onCLick="opener()"></i>');
         
   // Hide mobile submenu by default
-  $('#mobile-menu-nav .sub-menu').hide();
-})
-    
+  jQuery('#mobile-menu-nav .sub-menu').hide();
+}
+
 // Function for opening the mobile submenu
 function opener() {
     jQuery('#mobile-menu-nav .sub-menu').slideToggle(200);
