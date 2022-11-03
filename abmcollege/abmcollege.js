5ebfbb6034a33fbb664d65d2391752f35eb84697
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2022-11-03 09:57:59 +0000

    Update abmcollege.js

--- abmcollege/abmcollege.js
+++ abmcollege/abmcollege.js
@@ -205,7 +205,7 @@ Webflow.push(function () {
         // Freeze background scrolling when mobile menu is open
         $('.menu-mob-2').on('click',function() {
            if ($(this).hasClass("w--open")) {
-             $('.page-wrapper').css('overflow-y','auto')
-           } else {$('.page-wrapper').css('overflow-y','hidden')}
+             $('body').css('overflow-y','auto')
+           } else {$('body').css('overflow-y','hidden')}
         })
   })
