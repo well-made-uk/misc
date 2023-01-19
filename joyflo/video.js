2a9338d45ae4bf35ff65d5d0a23e98b4f163eadc
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-19 15:54:56 +0000

    Move to Joyflo folder; add min

--- joyflo/video.js
+++ joyflo/video.js
@@ -8,6 +8,27 @@ Webflow.push(function () {
     const ended = $(`[data-video-ended=${id}]`)
     const play = $(`[data-video-play=${id}]`)
 
+    $('[data-transport-play='+id+']').on('click',(event)=>{
+      $(video).trigger('play');
+      $(event.target).hide()
+      $(event.target).siblings().show()
+    })
+    $('[data-transport-pause='+id+']').on('click',(event)=>{
+      $(video).trigger('pause');
+      $(event.target).hide()
+      $(event.target).siblings().show()
+    })
+    $('[data-transport-unmute='+id+']').on('click',(event)=>{
+      $(video).prop('muted',false);
+      $(event.target).hide()
+      $(event.target).siblings().show()
+    })
+    $('[data-transport-mute='+id+']').on('click',(event)=>{
+      $(video).prop('muted',true);
+      $(event.target).hide()
+      $(event.target).siblings().show()
+    })
+
     // Set the video wrapper up
     $(ended).css("display", "flex").hide()
 
@@ -22,6 +43,9 @@ Webflow.push(function () {
           $(ended).fadeIn(300)
     });
   }
+
+
+
 
   // Set the section height
   // $('.video-parallax').css('height','calc(225vh + 560px + 4rem)')
