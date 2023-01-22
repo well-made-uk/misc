270297e742f8b1f7c408e0676e665d9bfa019b57
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-22 09:14:28 +0000

    Add loader remove

--- flowhost/script.js
+++ flowhost/script.js
@@ -1,9 +1,18 @@
 var Webflow = Webflow || [];
 Webflow.push(function () {
+
+
+
+
+
+  // Add panel expanders
   $('[data-fn-expand]').click((event)=>{
     target = $(event.target).attr('data-fn-expand')
     panel = $('[data-fn-panel='+target+']')
     $(panel).removeClass('collapsed')
+    $(panel).siblings('.gradient-cover').remove()
     $(event.target).remove()
   })
+
+  window.setTimeout($('.loader').fadeOut(500), 300)
 });
