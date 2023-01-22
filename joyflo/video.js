2a1b253ef5bd1e04a27f2722b867a0f43fbc8f0c
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-22 08:21:26 +0000

    vs

--- joyflo/video.js
+++ joyflo/video.js
@@ -1,53 +1,4 @@
-var Webflow = Webflow || [];
-Webflow.push(function () {
-  const videos = $('[data-video-id]')
-  for (let i = 0; i < videos.length; i++) {
-    const videoParent = videos[i]
-    const video = $(videoParent).children('video')
-    const id = $(videoParent).attr('data-video-id')
-    const ended = $(`[data-video-ended=${id}]`)
-    const play = $(`[data-video-play=${id}]`)
-
-    $('[data-transport-play='+id+']').on('click',(event)=>{
-      $(video).trigger('play');
-      $(event.target).hide()
-      $(event.target).siblings().show()
-    })
-    $('[data-transport-pause='+id+']').on('click',(event)=>{
-      $(video).trigger('pause');
-      $(event.target).hide()
-      $(event.target).siblings().show()
-    })
-    $('[data-transport-unmute='+id+']').on('click',(event)=>{
-      $(video).prop('muted',false);
-      $(event.target).hide()
-      $(event.target).siblings().show()
-    })
-    $('[data-transport-mute='+id+']').on('click',(event)=>{
-      $(video).prop('muted',true);
-      $(event.target).hide()
-      $(event.target).siblings().show()
-    })
-
-    // Set the video wrapper up
-    $(ended).css("display", "flex").hide()
-
-    // Replay video function
-    $(play).on('click',()=>{
-        $(video).trigger('play')
-        $(ended).fadeOut(300)
-    })
-
-    // End of video function
-    $(video).on('ended',()=>{
-          $(ended).fadeIn(300)
-    });
-  }
-
-
-  $('.video-inner').css('width',window.innerWidth).css('height',window.innerHeight)
-
-videoScroller = function (id,targetWidth,targetHeight) {
+function videoScroller(id,targetWidth,targetHeight) {
 
     const ogth = targetHeight
 
@@ -102,5 +53,54 @@ videoScroller = function (id,targetWidth,targetHeight) {
         video.css('width', zoomedWidth).css('height',zoomedHeight);
     });
 }
+
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  const videos = $('[data-video-id]')
+  for (let i = 0; i < videos.length; i++) {
+    const videoParent = videos[i]
+    const video = $(videoParent).children('video')
+    const id = $(videoParent).attr('data-video-id')
+    const ended = $(`[data-video-ended=${id}]`)
+    const play = $(`[data-video-play=${id}]`)
+
+    $('[data-transport-play='+id+']').on('click',(event)=>{
+      $(video).trigger('play');
+      $(event.target).hide()
+      $(event.target).siblings().show()
+    })
+    $('[data-transport-pause='+id+']').on('click',(event)=>{
+      $(video).trigger('pause');
+      $(event.target).hide()
+      $(event.target).siblings().show()
+    })
+    $('[data-transport-unmute='+id+']').on('click',(event)=>{
+      $(video).prop('muted',false);
+      $(event.target).hide()
+      $(event.target).siblings().show()
+    })
+    $('[data-transport-mute='+id+']').on('click',(event)=>{
+      $(video).prop('muted',true);
+      $(event.target).hide()
+      $(event.target).siblings().show()
+    })
+
+    // Set the video wrapper up
+    $(ended).css("display", "flex").hide()
+
+    // Replay video function
+    $(play).on('click',()=>{
+        $(video).trigger('play')
+        $(ended).fadeOut(300)
+    })
+
+    // End of video function
+    $(video).on('ended',()=>{
+          $(ended).fadeIn(300)
+    });
+  }
+
+
+  $('.video-inner').css('width',window.innerWidth).css('height',window.innerHeight)
 
 });
