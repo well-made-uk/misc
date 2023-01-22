2b53de2050669243f36923e0b83555b728ee5b32
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-22 09:22:26 +0000

    Add getUrlVars

--- flowhost/script.js
+++ flowhost/script.js
@@ -1,3 +1,19 @@
+// Get URL Vars
+function getUrlVars() {
+	var vars = [],
+		hash
+	const hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&')
+	for (var i = 0; i < hashes.length; i++) {
+		hash = hashes[i].split('=')
+		vars.push(hash[0])
+		vars[hash[0]] = hash[1]
+	}
+	return vars;
+}
+
+
+
+
 var Webflow = Webflow || [];
 Webflow.push(function () {
 
