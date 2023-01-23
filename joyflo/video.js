bcadf303eb4dceb9cbde23086e61af22cb0a57d3
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-23 11:09:54 +0000

    Update video.js

--- joyflo/video.js
+++ joyflo/video.js
@@ -61,7 +61,7 @@ function videoScroller(id,targetWidth,targetHeight) {
         if (window.innerHeight > 950) {
           marginOffset = (scrollTopPos * 0.1) * (window.innerHeight / 1080)
           bottomOffset = 0 - marginOffset
-        }
+        } else {marginOffset = 0;bottomOffset = 0}
 
         // set the sizes
         video.css('width', zoomedWidth).css('height',zoomedHeight).css('margin-bottom',marginOffset);
