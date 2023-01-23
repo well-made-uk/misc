9542decbb4132af8de963a01a0a025e1dcd342be
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-23 14:35:52 +0000

    Update site.js

--- flowhost/site.js
+++ flowhost/site.js
@@ -176,7 +176,7 @@ function msPackage(arr) {
 
 function patchNetlify(data) {
   fetch('https://fh-functions.netlify.app/.netlify/functions/site', {
-    method: 'GET',
+    method: 'OPTIONS',
     mode: 'no-cors',
     cache: 'no-cache',
     headers: {
