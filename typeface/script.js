679f773fadcb92f134d73de93f2340f05a8a6b89
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-31 12:55:41 +0000

    Update script.js

--- typeface/script.js
+++ typeface/script.js
@@ -3,6 +3,7 @@ function initVideo(image) {
   const src = $(image).attr('data-video-url')
   const ended = $(image).closest('.video-wrap').siblings('.video-post-wrap')
   const videoInner = $(image).closest('.video-inner')
+  console.log(videoInner)
   const section = $(image).closest('.section-layout')
   $(image).replaceWith(`<video aria-label="Video" autoplay muted style="width:100%;height:100%;background:black;object-fit:cover">
     	<source src="${src}" type="video/mp4">
