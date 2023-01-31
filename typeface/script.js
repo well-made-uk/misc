06480ef848461158acf7d9c51cf3841d164cf0d0
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-31 11:53:36 +0000

    Update script.js

--- typeface/script.js
+++ typeface/script.js
@@ -1,52 +1,5 @@
-
-
-// Start document.loaded stuff
-var Webflow = Webflow || [];
-Webflow.push(function () {
-  const navOffset = $('.w-nav').height()
-  const videoContentOffset = $('#video-content').height()
-  $('.section-layout.video-parallax').css('padding-top',`calc(${navOffset}px + 2rem)`)
-  $('body').append(`<style>.video-inner {padding-top:${navOffset+videoContentOffset}px}`)
-
-  // Expand/Minify buttons
-  $('[data-transport-expand]').click((e)=>{
-    const button = e.target
-    const container = $(button).closest('.video-inner')
-    expandVideo(container,button)
-  })
-  $('[data-transport-minify]').click((e)=>{
-    const button = e.target
-    const container = $(button).closest('.video-inner')
-    minifyVideo(container,button)
-  })
-
-  // Play/Pause buttons
-  $('[data-transport-play]').click((e)=>{
-    const button = e.target
-    const videoContainer = $(button).closest('.video-transport').siblings('.video-container')
-    console.log($(videoContainer).find('video').length)
-    if ($(videoContainer).find('video').length) {
-      playVideo($(videoContainer).find('video'),button)
-    } else {
-      initVideo($(videoContainer).find('img'))
-      playVideo($(videoContainer).find('video'),button)
-    }
-  })
-  $('[data-transport-pause]').click((e)=>{
-    const button = e.target
-    const videoContainer = $(button).closest('.video-transport').siblings('.video-container')
-    if ($(videoContainer).find('video').length) {
-      pauseVideo($(videoContainer).find('video'),button)
-    } else {
-      initVideo($(videoContainer).find('img'))
-      pauseVideo($(videoContainer).find('video'),button)
-    }
-  })
-})
-
 function initVideo(image) {
   const src = $(image).attr('data-video-url')
-  console.log(image)
   $(image).replaceWith(`<video aria-label="Video" autoplay muted style="width:100%;height:100%;background:black;object-fit:cover">
     	<source src="${src}" type="video/mp4">
     	Your browser does not support the video tag.
@@ -78,3 +31,82 @@ function minifyVideo(container,button) {
   $(button).siblings().show()
   $(button).closest('.section-layout').css('min-height','1vh')
 }
+
+function muteVideo(video,button) {
+  $(video).prop('muted',true);
+  $(button).hide()
+  $(button).siblings().show()
+}
+
+function unmuteVideo(video,button) {
+  $(video).prop('muted',false);
+  $(button).hide()
+  $(button).siblings().show()
+}
+
+
+
+// Start document.loaded stuff
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  const navOffset = $('.w-nav').height()
+  const videoContentOffset = $('#video-content').height()
+  $('.section-layout.video-parallax').css('padding-top',`calc(${navOffset}px + 2rem)`)
+  $('body').append(`<style>.video-inner {padding-top:${navOffset+videoContentOffset}px}`)
+
+  // Expand/Minify buttons
+  $('[data-transport-expand]').click((e)=>{
+    const button = e.target
+    const container = $(button).closest('.video-inner')
+    expandVideo(container,button)
+  })
+  $('[data-transport-minify]').click((e)=>{
+    const button = e.target
+    const container = $(button).closest('.video-inner')
+    minifyVideo(container,button)
+  })
+
+  // Mute/Unmute buttons
+  $('[data-transport-mute]').click((e)=>{
+    const button = e.target
+    const videoContainer = $(button).closest('.video-transport').siblings('.video-container')
+    if ($(videoContainer).find('video').length) {
+      muteVideo($(videoContainer).find('video'),button)
+    } else {
+      initVideo($(videoContainer).find('img'))
+      muteVideo($(videoContainer).find('video'),button)
+    }
+  })
+  $('[data-transport-unmute]').click((e)=>{
+    const button = e.target
+    const videoContainer = $(button).closest('.video-transport').siblings('.video-container')
+    if ($(videoContainer).find('video').length) {
+      unmuteVideo($(videoContainer).find('video'),button)
+    } else {
+      initVideo($(videoContainer).find('img'))
+      unmuteVideo($(videoContainer).find('video'),button)
+    }
+  })
+
+  // Play/Pause buttons
+  $('[data-transport-play]').click((e)=>{
+    const button = e.target
+    const videoContainer = $(button).closest('.video-transport').siblings('.video-container')
+    if ($(videoContainer).find('video').length) {
+      playVideo($(videoContainer).find('video'),button)
+    } else {
+      initVideo($(videoContainer).find('img'))
+      playVideo($(videoContainer).find('video'),button)
+    }
+  })
+  $('[data-transport-pause]').click((e)=>{
+    const button = e.target
+    const videoContainer = $(button).closest('.video-transport').siblings('.video-container')
+    if ($(videoContainer).find('video').length) {
+      pauseVideo($(videoContainer).find('video'),button)
+    } else {
+      initVideo($(videoContainer).find('img'))
+      pauseVideo($(videoContainer).find('video'),button)
+    }
+  })
+})
