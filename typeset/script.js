d32a1034abd8eb314aa319ab2d1a746e43a22a30
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-31 10:34:52 +0000

    Create script.js

---
+++ typeset/script.js
@@ -0,0 +1,8 @@
+
+
+// Start document.loaded stuff
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  const navOffset = $('.w-nav').height()
+  $('.section-layout.video-parallax').css('padding-top',`calc(${navOffset} + 2rem`)
+})
