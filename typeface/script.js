9bc1a8439bf067b87320e642ddc876e9ad72e309
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-01-31 11:43:38 +0000

    Update script.js

--- typeface/script.js
+++ typeface/script.js
@@ -24,7 +24,7 @@ Webflow.push(function () {
   $('[data-transport-play]').click((e)=>{
     const button = e.target
     const videoContainer = $(button).closest('.video-transport').siblings('.video-container')
-    if ($(videoContainer).find('video')) {
+    if ($(videoContainer).find('video').length() > 0) {
       playVideo($(videoContainer).find('video'),button)
     } else {
       initVideo($(videoContainer).find(img))
@@ -34,7 +34,7 @@ Webflow.push(function () {
   $('[data-transport-pause]').click((e)=>{
     const button = e.target
     const videoContainer = $(button).closest('.video-transport').siblings('.video-container')
-    if ($(videoContainer).find('video')) {
+    if ($(videoContainer).find('video').length() > 0) {
       pauseVideo($(videoContainer).find('video'),button)
     } else {
       initVideo($(videoContainer).find(img))
