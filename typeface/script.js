a00707ff259f0e9cba41bd75859d36e0a661aeef
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-02-05 18:44:15 +0000

    Update script.js

--- typeface/script.js
+++ typeface/script.js
@@ -133,7 +133,7 @@ function heroSetupMobile() {
   const videosrc = $(image).attr('video')
 
   function loadVideo() {
-    $(image).children().replaceWith(`<video controls autoplay class="video-container" style="max-width:100%;height:auto">
+    $(image).replaceWith(`<video controls autoplay class="video-container" style="max-width:100%;height:auto">
       <source src="${videosrc}" type="video/mp4">
       Your browser does not support the video tag.
     </video>`)
