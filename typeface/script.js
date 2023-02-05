72b0579b77dc6218d5a4c5b2c961d49f46329f96
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-02-05 15:19:55 +0000

    Update script.js

--- typeface/script.js
+++ typeface/script.js
@@ -110,7 +110,7 @@ function heroSetup() {
           $('.as-seen-on').slideUp(300)
           $('#video-hero').animate({'padding-bottom':'4rem','min-height':'0'},300)
           $(links).fadeOut(300)
-          $(videoInner).css('width','960px').css('height','520px')
+          $(videoInner).removeClass('expanded')
           $(videoInner).fadeOut(300,()=>{
             $(videoInner).remove()
             $('.w-nav').css('z-index','101')
