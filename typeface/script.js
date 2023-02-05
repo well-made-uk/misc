85783ce1a92d0577681cce42ae8477f35b165007
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-02-05 19:37:39 +0000

    Update script.js

--- typeface/script.js
+++ typeface/script.js
@@ -150,8 +150,8 @@ function heroSetupMobile() {
   })
   $('[control=open-modal]').click((e)=>{
     loadVideo()
-    console.log( $(e.target).is('.modal') )
-    if ( $(e.target).is('.modal') ) {
+    console.log( $(e.target).hasClass('modal') )
+    if ( $(e.target).hasClass('modal') ) {
       $(modal).find('.video-container').trigger('play')
     } else {
       $('#video-hero').find('.video-container').trigger('play')
