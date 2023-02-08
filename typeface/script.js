d8eeb4de4bc0f2518609a48f620c29ee16469964
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-02-08 15:14:19 +0000

    Update script.js

--- typeface/script.js
+++ typeface/script.js
@@ -291,6 +291,7 @@ Webflow.push(function () {
         const list = $(obj).siblings('[data-carousel-list]').children('[data-carousel]')
         let e = 0
         function carousel() {
+          console.log(`Create animation for ${$(list[e]).attr('data-carousel')}`)
           animation = loadLottie(obj,$(list[e]).attr('data-carousel'))
           $(list).removeClass('active')
           $(list[e]).addClass('active')
