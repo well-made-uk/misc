29d9109b044320a3fd585a75ea011e5314ea567d
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-02-08 09:27:16 +0000

    Update script.js

--- typeface/script.js
+++ typeface/script.js
@@ -244,7 +244,7 @@ Webflow.push(function () {
       if (connection == 'fast') {
         heroSetup()
       } else {
-        $('document').click(heroSetup)
+        $('document').click(()=>{heroSetup})
       }
     }
 
