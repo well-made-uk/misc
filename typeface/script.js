a2ea53ef9f122b6d66b6906f0f7b8dce053fd4e2
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-02-10 08:46:58 +0000

    .

--- typeface/script.js
+++ typeface/script.js
@@ -321,7 +321,7 @@ Webflow.push(function () {
             firstRun = false;
             $(list).on('click',(item)=>{
               e = $(item.target).parent().index(item.target)
-              console.log($(item.target))
+              console.log(item)
               animation.onComplete = ()=>{return}
               advanceCarousel(animation)
             })
