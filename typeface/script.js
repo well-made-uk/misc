38a05295a3e8a9abc4b62d4bafb12e62b62f42e9
Ben Wilde <90322602+well-made-uk@users.noreply.github.com>
2023-02-15 15:25:13 +0000

    Initial release

--- typeface/script.js
+++ typeface/script.js
@@ -141,17 +141,17 @@ function heroSetupMobile() {
     </video>`)
   }
 
-  $(controls).each((i, obj)=>{
+  $(controls).each((i, obj) => {
     const control = $(obj).attr('control')
-    switch(control) {
+    switch (control) {
       case 'close':
-      $(obj).click(()=>{
-        $(modal).fadeOut(300,()=>{$(modal).remove()})
-      })
+        $(obj).click(() => {
+          $(modal).fadeOut(300, () => { $(modal).remove() })
+        })
     }
   })
-  $('[control=open-modal]').click((e)=>{
-    if ( $(e.target).hasClass('.video-post-inner') ) {
+  $('[control=open-modal]').click((e) => {
+    if ($(e.target).hasClass('.video-post-inner')) {
       loadVideo(e.target)
     } else {
       loadVideo($(e.target).closest('.video-post-inner'))
@@ -161,98 +161,99 @@ function heroSetupMobile() {
 }
 
 function modalSetup() {
-    if (modalSetupComplete) {return}
+  if (modalSetupComplete) { return }
 
-    const section = $('#video-modal')
-    const image = $(section).find('.video-container')
+  const section = $('#video-modal')
+  const image = $(section).find('.video-container')
 
-    // Set video
-    $(image).replaceWith(`<video class="video-container">
+  // Set video
+  $(image).replaceWith(`<video class="video-container">
       <source src="${videosrc}" type="video/mp4">
       Your browser does not support the video tag.
     </video>`)
 
-    const video = $(section).find('.video-container')
-    const controls = $(section).find('[control]')
+  const video = $(section).find('.video-container')
+  const controls = $(section).find('[control]')
 
-    // Set controls
-    $(controls).each(function(i, obj) {
-      const control = $(obj).attr('control')
-      switch(control) {
-        case 'play':
-          $(obj).click(()=>{
-            $(video).trigger('play')
-            $(obj).hide()
-            $(obj).siblings().show()
-          })
+  // Set controls
+  $(controls).each(function(i, obj) {
+    const control = $(obj).attr('control')
+    switch (control) {
+      case 'play':
+        $(obj).click(() => {
+          $(video).trigger('play')
+          $(obj).hide()
+          $(obj).siblings().show()
+        })
         break;
-        case 'pause':
-          $(obj).click(()=>{
-            $(video).trigger('pause')
-            $(obj).hide()
-            $(obj).siblings().show()
-          })
+      case 'pause':
+        $(obj).click(() => {
+          $(video).trigger('pause')
+          $(obj).hide()
+          $(obj).siblings().show()
+        })
         break;
-        case 'mute':
-          $(obj).click(()=>{
-            $(video).prop('muted',true)
-            $(obj).hide()
-            $(obj).siblings().show()
-          })
+      case 'mute':
+        $(obj).click(() => {
+          $(video).prop('muted', true)
+          $(obj).hide()
+          $(obj).siblings().show()
+        })
         break;
-        case 'unmute':
-          $(obj).click(()=>{
-            $(video).prop('muted',false)
-            $(obj).hide()
-            $(obj).siblings().show()
-          })
+      case 'unmute':
+        $(obj).click(() => {
+          $(video).prop('muted', false)
+          $(obj).hide()
+          $(obj).siblings().show()
+        })
         break;
-        case 'close':
-          $(obj).click(()=>{
-            $(section).find('[control=pause]').click()
-            $(section).fadeOut(200)
-          })
-      }
-    })
-    $('[control=open-modal]').click(()=>{
-      $(section).fadeIn(300,()=>{$(section).find('[control=play]').click()})
-    })
-    $(video).on('ended',()=>{
-      $(section).fadeOut(300)
-    })
-    modalSetupComplete = true
-    $(section).fadeIn(300,()=>{$(video).trigger('play')})
-  }
+      case 'close':
+        $(obj).click(() => {
+          $(section).find('[control=pause]').click()
+          $(section).fadeOut(200)
+        })
+    }
+  })
+  $('[control=open-modal]').click(() => {
+    $(section).fadeIn(300, () => { $(section).find('[control=play]').click() })
+  })
+  $(video).on('ended', () => {
+    $(section).fadeOut(300)
+  })
+  modalSetupComplete = true
+  $(section).fadeIn(300, () => { $(video).trigger('play') })
+}
 
-function loadLottie(cont,src) {
+function loadLottie(cont, src) {
   return animation = lottie.loadAnimation({
-      container: cont,
-      renderer: 'svg',
-      loop: true,
-      autoplay: true,
-      path: src,
-      rendererSettings: {
-        scaleMode: 'noScale',
-        clearCanvas: true,
-        progressiveLoad: true,
-        hideOnTransparent: true
-      }
+    container: cont,
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: src,
+    rendererSettings: {
+      scaleMode: 'noScale',
+      clearCanvas: true,
+      progressiveLoad: true,
+      hideOnTransparent: true
+    }
   });
 }
 
 // Start document.loaded stuff
 var Webflow = Webflow || [];
-Webflow.push(function () {
+Webflow.push(function() {
 
-  (function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
+  (function(a) {
+    (jQuery.browser = jQuery.browser || {}).mobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)) })(navigator.userAgent || navigator.vendor || window.opera);
 
-  if ( jQuery.browser.mobile ) {
+  if (jQuery.browser.mobile) {
     // if ( $('#video-hero').length > 0 ) {
     //   $('.video-inner').remove()
     //   $('#video-hero .video-post-inner').css('display','block').css('opacity','1')
     //   $('.w-nav').css('z-index','101')
     // }
-    if ( $('#video-modal-mobile').length > 0 ) {
+    if ($('#video-modal-mobile').length > 0) {
       $('#video-modal').remove()
       heroSetupMobile()
     }
@@ -260,13 +261,13 @@ Webflow.push(function () {
     // if ( $('#video-hero').length > 0 ) {
     //   heroSetup()
     // }
-    $('[control=open-modal]').click(()=>{
-      if ( !modalSetupComplete ) {modalSetup()}
+    $('[control=open-modal]').click(() => {
+      if (!modalSetupComplete) { modalSetup() }
     })
   }
 
   if ($('[data-carousel-lottie]').length) {
-    $('[data-carousel-lottie]').each((e,obj)=>{
+    $('[data-carousel-lottie]').each((e, obj) => {
       const id = $(obj).attr('data-carousel-lottie')
       const lottieEl = $(obj)
       const listEl = $(`[data-carousel-list=${id}]`)
@@ -276,31 +277,30 @@ Webflow.push(function () {
   }
 
   // Lottie async
-  function elementScrolled(el)
-  {
+  function elementScrolled(el) {
     const docViewTop = $(window).scrollTop();
     const docViewBottom = docViewTop + $(window).height();
     const elTop = $(el).offset().top;
     return ((elTop <= docViewBottom) && (elTop >= docViewTop));
   }
 
-  let sc=0;
+  let sc = 0;
   $(window).scroll(function() {
-    if(sc == 0){
-      sc=1;
-      $('[data-lottie]').each((i,obj)=>{
+    if (sc == 0) {
+      sc = 1;
+      $('[data-lottie]').each((i, obj) => {
         $(obj).children().remove()
-        loadLottie(obj,$(obj).attr('data-lottie'))
+        loadLottie(obj, $(obj).attr('data-lottie'))
       })
     }
   })
-      
-  $('[data-carousel-lottie]').each((i,obj)=>{
+
+  $('[data-carousel-lottie]').each((i, obj) => {
     let done = false
     // This is then function used to detect if the element is scrolled into view
     $(window).scroll(function() {
-      if  (elementScrolled(obj)) {
-        if ( done == true) {return}
+      if (elementScrolled(obj)) {
+        if (done == true) { return }
         done = true
         const id = $(obj).attr('data-carousel-lottie')
         const list = $(`[data-carousel-list=${id}]`).children('[data-carousel]')
@@ -308,8 +308,31 @@ Webflow.push(function () {
         let e = -1
         let firstRun = true
         $(obj).children().remove()
-        
+
         let animation = lottie.loadAnimation({
+          container: obj,
+          renderer: 'svg',
+          loop: false,
+          autoplay: true,
+          path: $(list[e]).attr('data-carousel'),
+          rendererSettings: {
+            scaleMode: 'noScale',
+            clearCanvas: true,
+            progressiveLoad: true,
+            hideOnTransparent: true
+          }
+        });
+
+        function advanceCarousel(run) {
+          e++
+          if (e >= length) { e = 0 }
+          $(obj).children().remove()
+          animation.onComplete = () => {}
+          animation.destroy()
+          $(list).removeClass('active')
+          $(list[e]).addClass('active')
+
+          animation = lottie.loadAnimation({
             container: obj,
             renderer: 'svg',
             loop: false,
@@ -321,35 +344,13 @@ Webflow.push(function () {
               progressiveLoad: true,
               hideOnTransparent: true
             }
-        });
-        
-        function advanceCarousel(run) {
-          e++
-          if (e >= length) {e = 0}
-          $(obj).children().remove()
-          animation.onComplete = ()=>{}
-          animation.destroy()
-          $(list).removeClass('active')
-          $(list[e]).addClass('active')
-          
-          animation = lottie.loadAnimation({
-              container: obj,
-              renderer: 'svg',
-              loop: false,
-              autoplay: true,
-              path: $(list[e]).attr('data-carousel'),
-              rendererSettings: {
-                scaleMode: 'noScale',
-                clearCanvas: true,
-                progressiveLoad: true,
-                hideOnTransparent: true
-              }
           });
-          animation.onComplete = ()=>{animation.onComplete = ()=>{};advanceCarousel(true)}
-          
+          animation.onComplete = () => { animation.onComplete = () => {};
+            advanceCarousel(true) }
+
           if (firstRun) {
             firstRun = false;
-            animation.onEnterFrame = ()=>{setTimeout(()=>{$(obj).parent().css('height',$(obj).parent().height())},200)}
+            animation.onEnterFrame = () => { setTimeout(() => { $(obj).parent().css('height', $(obj).parent().height()) }, 200) }
             $(list).click(function() {
               e = $(this).index() - 1
               advanceCarousel(true)
